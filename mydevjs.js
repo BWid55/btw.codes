fdb401a216ff33687cbe7be00bc80a0d87615e7e
Brendan <brendan.widmer@gmail.com>
2021-09-19 15:31:15 -0500

    updating site with looma

---
+++ mydevjs.js
@@ -0,0 +1,16 @@
+var script = document.createElement('script');
+script.src = 'https://code.jquery.com/jquery-3.6.0.js';
+script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);
+
+// window.onload = function() {
+//     if (window.jQuery) {  
+//         // jQuery is loaded  
+//         alert("jQuery is loaded");
+//     } else {
+//         // jQuery is not loaded
+//         alert("jQuery is not loaded");
+//     }
+// }
+
+
