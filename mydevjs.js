3a6e5f65e97bf0c7b0c6698af53b261db0b96c83
Brendan <brendan.widmer@gmail.com>
2021-09-26 17:02:44 -0500

    big projects update

--- mydevjs.js
+++ mydevjs.js
@@ -1,16 +1,9 @@
-var script = document.createElement('script');
-script.src = 'https://code.jquery.com/jquery-3.6.0.js';
-script.type = 'text/javascript';
-document.getElementsByTagName('head')[0].appendChild(script);
+//\test jquery import success
 
 // window.onload = function() {
-//     if (window.jQuery) {  
-//         // jQuery is loaded  
+//     if (window.jQuery) {
 //         alert("jQuery is loaded");
 //     } else {
-//         // jQuery is not loaded
 //         alert("jQuery is not loaded");
 //     }
 // }
-
-
