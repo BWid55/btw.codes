9f80df893e35a0394739ed2f0047832ca6b7e2a4
Brendan <brendan.widmer@gmail.com>
2021-11-04 21:16:14 -0500

    trusted by pfeifletics logo added

--- mydevjs.js
+++ mydevjs.js
@@ -7,3 +7,12 @@
 //         alert("jQuery is not loaded");
 //     }
 // }
+
+// Project Show/Hide
+$(function() {
+    $('#project-selector').change(function(){
+        $('.projects').hide();
+        $('#' + $(this).val()).slideDown(1000);
+    });
+});
+// End Project Show/Hide
